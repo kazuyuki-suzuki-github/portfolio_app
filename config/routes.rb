83a7db0ea40dc6537e07693e4fb574861ed5e2fa
kazuyuki-suzuki-github <bfi.japan@gmail.com>
2020-10-18 14:01:41 +0900

    宿泊プラン機能実装
    宿泊プランモデルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   delete '/logout', to:'sessions#destroy'
   resources :users
   resources :companies
+  resources :plans
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
