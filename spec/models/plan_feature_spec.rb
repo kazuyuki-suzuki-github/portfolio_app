a4491ef357e7e44076eeca90183b0dc8b4d0796e
kazuyuki-suzuki-github <bfi.japan@gmail.com>
2020-12-17 14:58:10 +0900

    plan_featureモデルのバリデーションを作成

--- spec/models/plan_feature_spec.rb
+++ spec/models/plan_feature_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe PlanFeature, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with plans_id, features_id' do
+    pf = PlanFeature.new(plans_id:1, features_id:1)
+    expect(pf.valid?).to eq(true)
+  end
+
+  it 'is invalid without plans_id' do
+    pf = PlanFeature.new(plans_id:nil, features_id:1)
+    expect(pf.valid?).to eq(false)
+  end
+
+  it 'is invalid without features_id' do
+    pf = PlanFeature.new(plans_id:1, features_id:nil)
+    expect(pf.valid?).to eq(false)
+  end
 end
