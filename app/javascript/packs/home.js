329893f3c35fed3499298fcd57bb95fe59ca26e6
kazuyuki-suzuki-github <bfi.japan@gmail.com>
2020-12-20 18:26:04 +0900

    フロント実装　ヘッダ

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,5 +1,6 @@
 $(function() {
   $(".panel-btn").click(function(){
+    console.log('あいうえお')
     var clickPanel = $("+.panel",this);
     clickPanel.toggle();
     return false;
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root 'home#top'
   get '/login', to:'sessions#new'
   post '/login', to:'sessions#create'
-  delete '/logout', to:'sessions#destroy'
+  get '/logout', to:'sessions#destroy'
   resources :users
   resources :companies
   resources :plans
