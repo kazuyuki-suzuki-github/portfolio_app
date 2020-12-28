3a6cb4a19f084e159ee6b579c1f0c07aed17a5bd
kazuyuki-suzuki-github <bfi.japan@gmail.com>
2020-12-28 10:45:30 +0900

    検索でキーワードがない場合の挙動

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,12 @@
 class SearchController < ApplicationController
   def result
+    
     @search  = Search.new(
       prefecture_id:params[:search][:prefecture_id],
       checkin:params[:search][:checkin],
       checkout:params[:search][:checkout],
       people:params[:search][:people])
+
     if !@search.valid?
       @work_features = Feature.where(genre:1)
       @vacation_features = Feature.where(genre:2)
@@ -16,14 +18,31 @@ class SearchController < ApplicationController
     session[:checkin] = params[:search][:checkin]
     session[:checkout] = params[:search][:checkout]
     session[:people] = params[:search][:people]
-    @work_features = params[:search][:work_features]
-    @vacation_features = params[:search][:vacation_features]
-    @work_features.push(@vacation_features)
-    @search_features = @work_features.flatten!
-    @result_plans_id = Array.new
-  
-    @result = PlanFeature.select('plans_id, count(plans_id)').where(features_id: @search_features).group(:plans_id).order('count(plans_id) desc')
-    @result_plans_id = @result.map{|i| i.plans_id}
-    @search_result = Company.includes(:plans).where(plans:{id: @result_plans_id})
+
+    search_features = Array.new
+    search_features += params[:search][:work_features] if !params[:search][:work_features].nil?
+    search_features += params[:search][:vacation_features] if !params[:search][:vacation_features].nil?
+
+    if !search_features.empty?
+      # キーワードが一つでも選択された場合
+      target_plan_id =PlanFeature
+                              .select('plans_id, count(plans_id)')
+                              .where(features_id: search_features)
+                              .group(:plans_id)
+                              .order('count(plans_id) desc')
+      @search_result = Company.includes(:plans).where(
+                                                  companies: {prefecture_id: @prefecture_id},
+                                                  plans:
+                                                    {id: target_plan_id.map{|i| i.plans_id},
+                                                    sale_to: Date.parse(params[:search][:checkout])..Float::INFINITY})
+                                                .where.not(plans: {sale_from: Date.parse(params[:search][:checkin])-1..Float::INFINITY})
+    else
+      # キーワードが選択されていない場合
+      @search_result = Company.includes(:plans).where(
+                                                  companies: {prefecture_id: @prefecture_id},
+                                                  plans:
+                                                    {sale_to: Date.parse(params[:search][:checkout])..Float::INFINITY})
+                                                .where.not(plans: {sale_from: Date.parse(params[:search][:checkin])-1..Float::INFINITY})
+    end
   end
 end
